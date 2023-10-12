d94b85879318203ba623c63bdbc4e2ec122c0324
aslamcontact <aslam.mycontact@gmail.com>
2023-10-13 02:28:25 +0530

    add method to access the productImages api

--- src/main/java/com/ecom/product_catelog/businesslayer/ImageServiceExternal.java
+++ src/main/java/com/ecom/product_catelog/businesslayer/ImageServiceExternal.java
@@ -1,26 +1,33 @@
 package com.ecom.product_catelog.businesslayer;
 
-import com.ecom.product_catelog.exceptions.images.ImageMappeException;
-import jdk.dynalink.linker.LinkerServices;
+import com.ecom.product_catelog.exceptions.images.ImageExistException;
+import com.ecom.product_catelog.exceptions.images.ImageMapperExistException;
+import com.ecom.product_catelog.exceptions.images.ImageNotExistException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
+@PropertySource(value = "application.properties", ignoreResourceNotFound = true)
 @Service
 public class ImageServiceExternal {
     @Autowired
     RestTemplate template;
 
-    private  String url="http://194.163.40.229:8089";
-    public String saveImageMapper(String imageMapperId) throws ImageMappeException
+    @Value("${microservice.productimages.api.url}")
+      private  String url;
+  //  private  String url="http://url";
+    public String saveImageMapper(String imageMapperId) throws ImageMapperExistException
     {
         String path=url+"/api/v1/product/image/mapper/"+imageMapperId;
-        ResponseEntity<String> response;
+        ResponseEntity<String> response=ResponseEntity.ofNullable(null);
         HttpEntity<String> entity=new HttpEntity<>(new HttpHeaders());
         try {
 
@@ -34,14 +41,15 @@ public class ImageServiceExternal {
 
           }catch (HttpClientErrorException e)
             {
-               throw new ImageMappeException(e.getMessage(),e.getStatusCode());
+                if(e.getStatusCode().value()<500)throw new ImageMapperExistException( e.getMessage(),
+                                                                                      e.getStatusCode());
             }
         return response.getBody();
     }
     public String removeImageMapper(String imageMapperId)
     {
         String path=url+"/api/v1/product/image/mapper/"+imageMapperId;
-        ResponseEntity<String> response;
+        ResponseEntity<String> response=ResponseEntity.ofNullable(null);
         HttpEntity<String> entity=new HttpEntity<>(new HttpHeaders());
         try{
              response=template.exchange( path,
@@ -50,12 +58,73 @@ public class ImageServiceExternal {
                                          String.class
 
                                       );
-        }catch (HttpClientErrorException exception)
+        }catch (HttpClientErrorException e)
         {
-            throw new ImageMappeException(exception.getMessage(),exception.getStatusCode());
+            if(e.getStatusCode().value()<500)throw new ImageMapperExistException( e.getMessage(),
+                                                                                  e.getStatusCode());
         }
         return response.getBody();
     }
+   public byte[] getImageFromMapper( String imageMapperId,
+                                     String category
+                                  )
+   {
+       String path=url+"/api/v1/product/image/mapper/"+imageMapperId+"/"+category;
+       HttpHeaders httpHeaders=new HttpHeaders();
+       ResponseEntity<byte[]> response=ResponseEntity.ofNullable(null);
+       //  httpHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
+       HttpEntity<String> entity=new HttpEntity<>(httpHeaders);
+
+       try {
+           response = template.exchange(
+                                          path,
+                                          HttpMethod.GET,
+                                          entity,
+                                          byte[].class
+                                        );
+       }catch (HttpClientErrorException e)
+       {
+           if(e.getStatusCode().value()<500)throw new ImageNotExistException( e.getStatusCode(),
+                                                                              e.getMessage());
+
+       }
+
+
+       return  response.getBody();
+   }
+public String setImageToMapper(String imageMapperId,
+                               String category ,
+                               MultipartFile image){
+
+        String path = url + "/api/v1/product/image/mapper/" + imageMapperId + "/" + category;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        ResponseEntity<String> response=ResponseEntity.ofNullable(null);
+
+        MultiValueMap<String,Resource> body = new LinkedMultiValueMap<>();
+        body.add("image",image.getResource());
+
+        HttpEntity<MultiValueMap<String,Resource>> entity;
+        entity = new HttpEntity<>(body, headers);
+        try {
+
+
+            response = template.postForEntity(path,
+                    entity,
+                    String.class);
+        }catch (HttpClientErrorException e)
+        {
+            if(e.getStatusCode().value()<500) throw new ImageExistException(  e.getStatusCode(),
+                                                                              e.getMessage());
+        }
+
+        return response.getBody();
+
+}
+
+
+
+
 
 
 
--- src/main/java/com/ecom/product_catelog/exceptions/images/ImageMapperExistException.java
+++ src/main/java/com/ecom/product_catelog/exceptions/images/ImageMapperExistException.java
@@ -2,18 +2,18 @@ package com.ecom.product_catelog.exceptions.images;
 
 import org.springframework.http.HttpStatusCode;
 
-public class ImageMappeException extends RuntimeException{
+public class ImageMappeExistException extends RuntimeException{
     private final String apiErrorMessage;
     static final String message="ImageMapper Exist";
     private final HttpStatusCode httpStatusCode;
 
-    public ImageMappeException(String apiErrorMessage, HttpStatusCode httpStatusCode) {
+    public ImageMappeExistException(String apiErrorMessage, HttpStatusCode httpStatusCode) {
         super(message);
         this.apiErrorMessage = apiErrorMessage;
         this.httpStatusCode=httpStatusCode;
     }
 
-    public ImageMappeException(Throwable cause, String apiErrorMessage, HttpStatusCode httpStatusCode) {
+    public ImageMappeExistException(Throwable cause, String apiErrorMessage, HttpStatusCode httpStatusCode) {
         super(message, cause);
         this.apiErrorMessage =apiErrorMessage;
         this.httpStatusCode=httpStatusCode;
